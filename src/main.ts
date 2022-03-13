020f48c8ef3a4d376327c840e8e3ffe4fa8b064c
ejgandelaberon <ejgandelaberon@yahoo.com>
2022-03-13 09:57:11 +0800

    enable cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  //const globalPrefix = 'api';
+  //app.setGlobalPrefix(globalPrefix);
+  const port = process.env.port || 3000;
+  await app.listen(3000, () => {
+    console.log('Listening at http://localhost:' + port);
+  });
 }
 bootstrap();
